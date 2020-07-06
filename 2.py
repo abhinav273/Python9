5d0297823db02a282cd8b9b098c69e2e31999884
Code With Abhinav <67705141+abhinav273@users.noreply.github.com>
2020-07-06 13:50:52 +0530

    Add files via upload

--- 2.py
+++ 2.py
@@ -2,8 +2,8 @@ n = int(input())
 k = n - 1
 for i in range(0,n):
     for j in range(0,k):
-        print(end=" ")
+        print(end="  ")
     k = k - 1
     for j in range(0, i+1):
-        print("*", end=" ")
+        print("* ", end="  ")
     print("\n")
